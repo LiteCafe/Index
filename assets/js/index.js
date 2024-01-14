a75e4c3ccd6b79a9ebcc11959664391bb6147077
ImJingLan <imjinglan@126.com>
2024-01-14 14:56:06 +0800

    新的主页，添加了Meta标签

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,64 @@
-const endToast = bootstrap.Toast.getOrCreateInstance(
-    document.getElementById("endToast")
+const VIToast = bootstrap.Toast.getOrCreateInstance(
+    document.getElementById("VIToast")
 );
 
-//展示endToast
-function displayEndToast() {
-    endToast.show();
+VIToast.show();
+
+function getPing() {
+    console.log(new Date());
+    let status = document.getElementById("status");
+    let version = document.getElementById("version");
+
+    let online_players = document.getElementById("online_players");
+    let max_players = document.getElementById("max_players");
+    let player_list = document.getElementById("player_list");
+    var playerList = "";
+
+    fetch("https://api.lite.cafe/api/getPing")
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.code == 1 || data.data == false) {
+                if (!errorShow) {
+                    errorToast.show();
+                    errorShow = true;
+                } else {
+                }
+            } else {
+                errorToast.hide();
+                errorShow = false;
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching JSON:", error);
+            errorToast.show();
+        });
+}
+window.onload = () => {
+    getPing();
+};
+
+navheader = document.getElementById("navbar");
+navLogo = document.getElementById("nav-logo");
+navbar = document.getElementById("navbar");
+
+btt_span = document.getElementById("back-to-top");
+
+if (window.scrollY > 0) {
+    btt_span.style.opacity = "1";
+} else {
+    btt_span.style.opacity = "0";
 }
 
-window.onload = () => {};
+addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add("navbar-scrolled");
+        //btt_span.style.display = "block";
+        btt_span.style.opacity = "1";
+    } else {
+        navbar.classList.remove("navbar-scrolled");
+        //btt_span.style.display = "none";
+        btt_span.style.opacity = "0";
+    }
+});
+
+function back_to_top() {}
